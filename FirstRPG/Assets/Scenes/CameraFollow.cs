c7f851a1fd115238e82b563d2b06074f03bb86e0
poncerapha <38406330+poncerapha@users.noreply.github.com>
2018-07-17 11:50:21 -0300

    dasdas

--- FirstRPG/Assets/Scenes/CameraFollow.cs
+++ FirstRPG/Assets/Scenes/CameraFollow.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
-    public GameObject player;
+    GameObject player;
 
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
-       
-		
+
 	}
 	
 	// Update is called once per frame
-	void LateUpdate () {
-        
-		
+	void Update () {
+        transform.position = player.transform.position;
 	}
 }
